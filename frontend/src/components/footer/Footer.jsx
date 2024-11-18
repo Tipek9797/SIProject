e2b79873e9bb91acb9dbd6da4503bc1f8853463b
Samuel Gergel <samuel.gergel@student.ukf.sk>
2024-11-18 13:01:19 +0100

    Updated: footer, Added: icons

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
-// src/components/footer/Footer.jsx
 import React from 'react';
+import { PiGlobe, PiFacebookLogo, PiInstagramLogo } from 'react-icons/pi';  // Import ikony glóbusu a ostatných ikon
+
 import './footer.css';
 
 const Footer = () => {
@@ -16,6 +17,19 @@ const Footer = () => {
                     <p>Email: <a href="mailto:info@skola.sk">info@skola.sk</a></p>
                     <p>Telefón: <a href="tel:+421123456789">+421 123 456 789</a></p>
                     <p>Adresa: Tr. A. Hlinku 1, 949 01 Nitra, Slovensko</p>
+                    <div className="social-icons">
+                        {/* Zmenená ikona Discordu na PiGlobe */}
+                        <a href="https://www.fpvai.ukf.sk/sk/" target="_blank" rel="noopener noreferrer">
+                            <PiGlobe size={30} />
+                        </a>
+                        {/* Facebook a Instagram ikony */}
+                        <a href="https://www.facebook.com/fpvai.ukf" target="_blank" rel="noopener noreferrer">
+                            <PiFacebookLogo size={30} />
+                        </a>
+                        <a href="https://www.instagram.com/fpvai.ukf/" target="_blank" rel="noopener noreferrer">
+                            <PiInstagramLogo size={30} />
+                        </a>
+                    </div>
                 </div>
             </div>
         </footer>
