01019aec85487b7ecec8a6e8ec1030037bbe648d
Samuel Gergel <samuel.gergel@student.ukf.sk>
2024-12-06 12:33:43 +0100

    Added: home page Updated: footer

--- frontend/src/pages/homePage/Home.jsx
+++ frontend/src/pages/homePage/Home.jsx
@@ -20,34 +20,36 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className="info">
-                <h2>Aké výhody prináša účasť na ŠVK ?</h2>
-                <ul>
-                    <li>Účasť na konferencii je zadarmo</li>
-                    <li>Možnosť získať ocenenie</li>
-                    <li>Výstupom je publikácia príspevku</li>
-                    <li>Získať nové kontakty</li>
-                </ul>
-            </div>
-
-            <div className="info2">
-                <h3>TAK NEVÁHAJTE!</h3>
-                <p1>a zúčastnite sa študentskej vedeckej konferencie, ktorá je súťažnou prehliadkou vedeckých prác študentov</p1>
-                <div className="details-container">
-                    <p2>Termín konania: To be announced</p2>
-                    <p2>Miesto konania: UKF v Nitre</p2>
+            <div className="info-container">
+                <div className="info">
+                    <h2>Aké výhody prináša účasť na ŠVK ?</h2>
+                    <ul>
+                        <li>Účasť na konferencii je zadarmo</li>
+                        <li>Možnosť získať ocenenie</li>
+                        <li>Výstupom je publikácia príspevku</li>
+                        <li>Získať nové kontakty</li>
+                    </ul>
                 </div>
-            </div>
 
-            <div className="info3">
-                <h4>Čo musíte urobiť aby ste sa zúčastnili ?</h4>
-                <ul>
-                    <li>Vybrať si školiteľa a dohodnúť sa s ním na téme vedeckej práce</li>
-                    <li>Predložiť vedeckú prácu do [Dátum]</li>
-                    <li>Prácu verejne prezentovať a obhájiť v príslušnej sekcii pred odbornou komisiou</li>
-                    <li>Všetky obhájené práce budú publikované v zborníku recenzovaných prác s názvom Študentská vedecká konferencia 2025</li>
-                    <li>V každej sekcii budú najlepšie práce ocenené diplomom a mimoriadnym štipendiom alebo vecnou cenou</li>
-                </ul>
+                <div className="info2">
+                    <h3>TAK NEVÁHAJTE!</h3>
+                    <p>a zúčastnite sa študentskej vedeckej konferencie, ktorá je súťažnou prehliadkou vedeckých prác študentov</p>
+                    <div className="details-container">
+                        <p>Termín konania: To be announced</p>
+                        <p>Miesto konania: UKF v Nitre</p>
+                    </div>
+                </div>
+
+                <div className="info3">
+                    <h4>Čo musíte urobiť aby ste sa zúčastnili ?</h4>
+                    <ul>
+                        <li>Vybrať si školiteľa a dohodnúť sa s ním na téme vedeckej práce</li>
+                        <li>Predložiť vedeckú prácu do [Dátum]</li>
+                        <li>Prácu verejne prezentovať a obhájiť v príslušnej sekcii pred odbornou komisiou</li>
+                        <li>Všetky obhájené práce budú publikované v zborníku recenzovaných prác s názvom Študentská vedecká konferencia 2025</li>
+                        <li>V každej sekcii budú najlepšie práce ocenené diplomom a mimoriadnym štipendiom alebo vecnou cenou</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
