86a289dd9369775146d80a39f11051541e3a50d0
Jakub T <Jakubt2012@gmail.com>
2025-01-11 18:06:43 +0100

    Update To My_works v3. Frontend - Connect To Database.

--- frontend/src/components/work-dialog/WorkInfoDialog.jsx
+++ frontend/src/components/work-dialog/WorkInfoDialog.jsx
@@ -4,45 +4,48 @@ import { Divider } from "primereact/divider";
 import { Rating } from "primereact/rating";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import {InputTextarea, ScrollPanel} from "../index";
 
-export default function WorkInfoDialog({ visible, onHide, ratings, footer }) {
+export default function WorkInfoDialog({ visible, onHide, data, ratings}) {
     return (
         <Dialog
-            header="Názov práce"
+            header={data.name}
             visible={visible}
             className="work-dialog-details"
-            footer={footer}
             maximizable
-            style={{ width: "50vw" }}
+            style={{width: "50vw"}}
             onHide={onHide}
         >
-            <Divider />
+            <Divider/>
             <div className="work-details-text">
+                <label>Názov Konferencie Názov Konferencie Názov Konferencie Názov Konferencie</label><br/>
                 <div className="work-details-row">
-                    <h2>Škola</h2>
-                    <Divider layout="vertical" />
-                    <label htmlFor="school">Univerzita Konštantína Filozofa v Nitre</label>
+                    <h2>Škola</h2><i className="pi pi-angle-right"></i>
+                    <Divider layout="vertical"/>
+                    <label htmlFor="school">{data.users[0].school.name}</label>
                 </div>
                 <div className="work-details-row">
-                    <h2>Katedra</h2>
-                    <Divider layout="vertical" />
-                    <label htmlFor="department">Katedra Informatiky</label>
+                    <h2>Fakulta</h2><i className="pi pi-angle-right"></i>
+                    <Divider layout="vertical"/>
+                    <label htmlFor="department">{data.users[0].faculty.name}</label>
                 </div>
                 <div className="work-details-row">
-                    <h2>Odbor</h2>
-                    <Divider layout="vertical" />
-                    <label htmlFor="field">Aplikovaná Informatika</label>
+                    <h2>Hodnotenie</h2><i className="pi pi-angle-right"></i>
+                    <Divider layout="vertical"/>
+                    <Rating value={data.reviews[0].rating} readOnly cancel={false}/>
                 </div>
                 <div className="work-details-row">
-                    <h2>Hodnotenie</h2>
-                    <Divider layout="vertical" />
-                    <Rating value={4} readOnly cancel={false} />
+                    <h2>Posudok</h2><i className="pi pi-angle-down"></i>
+                    <Divider layout="vertical"/>
                 </div>
+                <ScrollPanel className="scrollP">
+                    <InputTextarea className="inpTextArea font-bold" disabled autoResize value={data.reviews[0].comment}/>
+                </ScrollPanel>
             </div>
             <div className="rating-table-container">
                 <DataTable value={ratings} className="p-datatable-sm" showGridlines>
-                    <Column className="rating-column" field="pros" header="Pozitíva +" />
-                    <Column className="rating-column" field="cons" header="Negatíva -" />
+                    <Column className="rating-column" field="PRO" header="Pozitíva +"/>
+                    <Column className="rating-column" field="CON" header="Negatíva -"/>
                 </DataTable>
             </div>
         </Dialog>
